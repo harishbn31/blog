8887ccc41801a458d928858a219a0ac780ee32ab
harish <harishbn31@gmail.com>
2017-08-30 17:18:05 +0530

    validation added

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,4 @@
 class Article < ApplicationRecord
+	belongs_to :category
+	validates_presence_of :title, :body,:author_id,:category_id
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,5 @@
 class Category < ApplicationRecord
+has_many :articles
+validates_presence_of :name
+
 end
