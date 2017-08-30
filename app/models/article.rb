fcb85deebf5653db742aeb4c2b75ad8d72feac14
harish <harishbn31@gmail.com>
2017-08-30 17:41:53 +0530

    created author model and validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
 	belongs_to :category
+	belongs_to :author
 	validates_presence_of :title, :body,:author_id,:category_id
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170830114456) do
+ActiveRecord::Schema.define(version: 20170830120925) do
 
   create_table "articles", force: :cascade do |t|
     t.text "title"
     t.text "body"
     t.integer "category_id"
     t.integer "author_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "authors", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
